a1c1446eafe46d1bb71f2dd74fed38675bc23186
Marc Pérez <marcperez2000@gmail.com>
2012-09-24 12:21:58 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listnr::Application.config.secret_token = '81080a60e47a1c934c7b54263b4b59f1b47abe9efaf02c6ecb9889aa37b9a4ec1a28140d2d6c2537150b4740bc97f2e44b507b543b56b8f58259cab87586a105'
